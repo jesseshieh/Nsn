0f1635f1755291229fc3817a5d9fd0645dc30e37
Aaron Price <coding.aaronp@gmail.com>
2018-01-23 20:52:51 -0800

    Add comma

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :nsn, NsnWeb.Endpoint,
   url: [host: "example.com", port: 80],
   load_from_system_env: true,
   server: true,
-  secret_key_base: "${SECRET_KEY_BASE}"
+  secret_key_base: "${SECRET_KEY_BASE}",
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
